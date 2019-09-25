67abe258b971e213a4dc8b4ca7f73a88dd797706
Li1353665264 <1353665264@qq.com>
2019-09-25 09:23:28 +0800

    a

--- application/index/controller/Outbound.php
+++ application/index/controller/Outbound.php
@@ -284,6 +284,7 @@ class Outbound extends Controller {
                 $cats[$k]['j']=0;
             }
         }
+        dump($cats);exit;
         return view('to_examine_show',['rows'=>$rows,'cats'=>$cats,'id'=>$id,'cks'=>$cks,'cabinet'=>$cabinet]);
     }
     //出库修改订单
