3a354672d99e0afc50f0c57337a1aee726822de2
Li1353665264 <1353665264@qq.com>
2019-10-11 14:40:00 +0800

    c

--- application/index/controller/Chaoji.php
+++ application/index/controller/Chaoji.php
@@ -13,31 +13,6 @@ class chaoji extends Controller {
         return view('index');
     }
     /**
-     * 进销存
-     */
-    public function jxc(){
-        $row=db('jxc')
-        ->where('is_del',0)
-        ->select();
-        return view('jxc',['row'=>$row]);
-    }
-    /**
-     * 进销存删除
-     */
-    public function jxc_sc($id){
-        $row=db('jxc')
-        ->where('id',$id)
-        ->update([
-            'is_del'=>1,
-            'del_time'=>time()
-        ]);
-        if($row){
-            return redirect('jxc');
-        }else{
-            $this->error('删除失败');
-        }
-    }
-    /**
      * 进销存详细
      */
     public function jxc_xx(){
@@ -51,7 +26,7 @@ class chaoji extends Controller {
         // 初期数
         $cq_num=db('record')
         ->field('hw_name,SUM(early_stage) early_stage')
-        ->where("task = '到货入库'")
+        ->where("task = '到货入库' or task = '其他入库' or task = '调拨入库'")
         ->where("hw_name <> ''")
         ->group('hw_name')
         ->select();
@@ -104,43 +79,6 @@ class chaoji extends Controller {
         ->where('hw_name',$name)
         ->select();
         return view('jxc_goods_xx',['res'=>$res]);
-    }
-    /**
-     * 创建进销存
-     */
-    public function create_jxc(){
-        $r=db('record')
-            ->where('jxc_id',0)
-            ->select();
-        if(!empty($r)){
-            Db::startTrans();;
-            try{
-                $data['create_time']=time();
-                $getid=db('jxc')
-                    ->insertGetId($data);
-
-                $re=db('record')
-                    ->where('jxc_id',0)
-                    ->update([
-                        'jxc_id'=>$getid
-                    ]);
-                if($re && $getid) {
-                    // 提交事务
-
-                    Db::commit();
-                    return redirect('Chaoji/jxc');
-                }
-
-            } catch (\Exception $e) {
-                $this->error('创建失败');
-
-                Db::rollback();
-            }
-        }else{
-            $this->error('已经是最新数据');
-        }
-
-
     }
     /**
      * 密码判断
@@ -270,6 +208,130 @@ class chaoji extends Controller {
             $this->error('清除成功');
         }else{
             $this->error('清除失败');
+        }
+    }
+    /**
+     * 进销存导出
+     */
+    public function outExcel(){
+        $ms=$this->qx();
+        if($ms==0){
+            $this->error('警告：越权操作');
+        }
+        $jxc_xx=array();
+        // 结存数
+        $jc_num=db('rukuform_xq')
+            ->field('product_name,SUM(rk_nums) rk_nums')
+            ->group('product_name')
+            ->where('state',1)
+            ->select();
+        // 初期数
+        $cq_num=db('record')
+            ->field('hw_name,SUM(early_stage) early_stage')
+            ->where("task = '到货入库' or task = '其他入库' or task = '调拨入库'")
+            ->where("hw_name <> ''")
+            ->group('hw_name')
+            ->select();
+        // 出入库详细
+        $xx=db('record')
+            ->field('hw_name,SUM(dh_ruku) dh_ruku,SUM(db_ruku) db_ruku,SUM(qt_ruku) qt_ruku,SUM(xx_chuku) xx_chuku,SUM(db_chuku) db_chuku,SUM(qt_chuku) qt_chuku')
+            ->where("hw_name <> ''")
+            ->group('hw_name')
+            ->select();
+        foreach($cq_num as $k=>$v){
+            $jxc_xx[$k]['early_stage']=$v['early_stage'];
+        }
+        foreach($jc_num as $k=>$v){
+            $jxc_xx[$k]['rk_nums']=$v['rk_nums'];
+        }
+        foreach($xx as $k=>$v){
+            $jxc_xx[$k]['hw_name']=$v['hw_name'];
+            $jxc_xx[$k]['dh_ruku']=$v['dh_ruku'];
+            $jxc_xx[$k]['db_ruku']=$v['db_ruku'];
+            $jxc_xx[$k]['qt_ruku']=$v['qt_ruku'];
+            $jxc_xx[$k]['xx_chuku']=$v['xx_chuku'];
+            $jxc_xx[$k]['db_chuku']=$v['db_chuku'];
+            $jxc_xx[$k]['qt_chuku']=$v['qt_chuku'];
+        }
+        if(!empty($jxc_xx)){
+            Vendor('PHPExcel.PHPExcel');
+            Vendor('PHPExcel.PHPExcel.IOFactory');
+            $phpExcel = new \PHPExcel();
+            $phpExcel->setActiveSheetIndex(0)
+                ->setCellValue('A1', '产品')
+                ->setCellValue('B1', '初期数量')
+                ->setCellValue('C1', '到货入库')
+                ->setCellValue('D1', '调拨入库')
+                ->setCellValue('E1', '其他入库')
+                ->setCellValue('F1', '销售出库')
+                ->setCellValue('G1', '调拨出库')
+                ->setCellValue('H1', '其他出库')
+                ->setCellValue('I1', '结存');
+            $len = count($jxc_xx);
+            for($i = 0 ; $i < $len ; $i++){
+                $v = $jxc_xx[$i];
+                $rownum = $i+2;
+                $phpExcel->getActiveSheet()->setCellValue('A' . $rownum, $v['hw_name']);
+                $phpExcel->getActiveSheet()->setCellValue('B' . $rownum, $v['early_stage']);
+                $phpExcel->getActiveSheet()->setCellValue('C' . $rownum, $v['dh_ruku']);
+                $phpExcel->getActiveSheet()->setCellValue('D' . $rownum, $v['db_ruku']);
+                $phpExcel->getActiveSheet()->setCellValue('E' . $rownum, $v['qt_ruku']);
+                $phpExcel->getActiveSheet()->setCellValue('F' . $rownum, $v['xx_chuku']);
+                $phpExcel->getActiveSheet()->setCellValue('G' . $rownum, $v['db_chuku']);
+                $phpExcel->getActiveSheet()->setCellValue('H' . $rownum, $v['qt_chuku']);
+                $phpExcel->getActiveSheet()->setCellValue('I' . $rownum, $v['rk_nums']);
+            }
+            $phpExcel->setActiveSheetIndex(0);
+            $filename=date('Y-m-d',time()).'.xlsx';
+            $objWriter=\PHPExcel_IOFactory::createWriter($phpExcel,'Excel2007');
+            $filePath =$filename;
+            $objWriter->save($filePath);
+            if(!file_exists($filePath)){
+                $response = array(
+                    'status' => 'false',
+                    'url' => '',
+                    'token'=>''
+                );
+            }else{
+                $response = array(
+                    'status' => true,
+                    'url' => $filename,
+                );
+            }
+        }else{
+            $response = array(
+                'status' => 'false',
+                'url' => '',
+                'token'=>''
+            );
+        }
+        exit(json_encode($response));
+    }
+    /**
+     * 下载进销存表格
+     */
+    public function download(){
+        $fileName = date('Y-m-d',time()).'.xlsx';
+        $path = ROOT_PATH."\public/".$fileName;
+        if(!file_exists($path)){
+            header("HTTP/1.0 404 Not Found");
+            exit;
+        }else{
+            $file = @fopen($path,"r");
+            if(!$file){
+                header("HTTP/1.0 505 Internal server error");
+                exit;
+            }
+            header("Content-type: application/octet-stream");
+            header("Accept-Ranges: bytes");
+            header("Accept-Length: ".filesize($path));
+            header("Content-Disposition: attachment; filename=" . $fileName);
+            while(!feof($file)){
+                echo fread($file,2048);
+            }
+            fclose($file);
+            unlink($path);
+            exit();
         }
     }
 }
